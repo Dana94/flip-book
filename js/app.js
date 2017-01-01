5120f34c5655b6786e57cf0ad8b78c8fd0417df7
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-01-01 17:27:51 -0500

    second figure will move until stop button is clicked

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 
+let images = ["<img class=\"img-responsive center-block\" src=\"images/new-up.png\">",
+			  "<img class=\"img-responsive center-block\" src=\"images/new-down.png\">"];
+//first part
+
 let button1 = document.getElementById('start-stop-1');
 
 let spot1 = document.getElementById('img-place-1');
 
-
-// find an easier way to do this
-button1.onclick = function(){
+function change1(){
 	
-	if (spot1.innerHTML == "<img class=\"img-responsive center-block\" src=\"images/new-up.png\">"){
-		spot1.innerHTML = "<img class=\"img-responsive center-block\" src=\"images/new-down.png\">";
+	if (spot1.innerHTML == images[0]){
+		spot1.innerHTML = images[1];
 	}
 	else{
-		spot1.innerHTML = "<img class=\"img-responsive center-block\" src=\"images/new-up.png\">";
+		spot1.innerHTML = images[0];
 	}
-}
+	
+} 
 
+
+button1.addEventListener('click', change1);
+
+
+//second part
 
 let start = document.getElementById('start');
 
@@ -22,25 +30,29 @@ let stop = document.getElementById('stop');
 
 let spot2 = document.getElementById('img-place-2');
 
-function change(){
+function change2(){
 	
-	//need to have time lapse established, too fast now
-	while(true){
-		if (spot2.innerHTML == "<img class=\"img-responsive center-block\" src=\"images/new-up.png\">"){
-			spot2.innerHTML = "<img class=\"img-responsive center-block\" src=\"images/new-down.png\">";
-		}
-		else{
-			spot2.innerHTML = "<img class=\"img-responsive center-block\" src=\"images/new-up.png\">";
-		}
+	if (spot2.innerHTML == images[0]){
+		spot2.innerHTML = images[1];
+	}
+	else{
+		spot2.innerHTML = images[0];
 	}
 	
 } 
 
-start.addEventListener('click', change);
+let timer;
 
+function startTimer(){
+	timer = window.setInterval(change2, 300);
+}
 
-stop.onclick = function(){
-	start.remove
+function stopTimer(){
+	window.clearInterval(timer);
 }
 
 
+
+start.addEventListener('click', startTimer);
+
+stop.addEventListener('click', stopTimer);
